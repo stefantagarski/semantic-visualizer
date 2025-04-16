08c965bd1c28515535eedf1138c2db4c730f5674
stefantagarski <stefantagarski@gmail.com>
2025-04-16 14:40:15 +0200

    initial commit

---
+++ src/main/java/com/semantic/semanticvisualizer/SemanticVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.semantic.semanticvisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticVisualizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemanticVisualizerApplication.class, args);
+    }
+
+}
