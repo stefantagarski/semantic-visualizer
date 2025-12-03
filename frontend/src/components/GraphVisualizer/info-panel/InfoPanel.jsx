119f07b8231ff589d3a84e999dcc643511f60b27
David Hristov <113634059+davidhristov59@users.noreply.github.com>
2025-12-03 03:17:23 +0100

    [FTR] added Tailwind , changes in the Vite configuration, packages and config files

--- frontend/src/components/GraphVisualizer/info-panel/InfoPanel.jsx
+++ frontend/src/components/GraphVisualizer/info-panel/InfoPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './InfoPanel.css';
 
-const InfoPanel = ({ nodeId, onClose }) => {
+const InfoPanel = ({ nodeId, label, onClose }) => {
     if (!nodeId) return null;
 
     return (
@@ -15,6 +15,11 @@ const InfoPanel = ({ nodeId, onClose }) => {
                 >
                     ×
                 </button>
+            </div>
+
+            <div className="data-section">
+                <div className="data-label">LABEL</div>
+                <div className="data-value">{label}</div>
             </div>
 
             <div className="data-section">
